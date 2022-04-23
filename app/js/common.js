a580139db7590f3345ccd01fd8d82cb5db2e3d8d
Ozodbek <o.jumaev@safenetpay.com>
2022-04-23 16:13:41 +0500

    fix all desktop page

--- app/js/common.js
+++ app/js/common.js
@@ -13,3 +13,57 @@
         navOverlay.style = 'display: none';
     }
 })();
+
+// Swiper slider
+
+
+
+  (function () {
+
+    let swiper = new Swiper(".swiper", {
+        loop: true,
+        slidesPerView: "auto",
+        centeredSlides: false,
+        spaceBetween: 30,
+        grabCursor: true,
+        breakpoints: {
+          1200: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+            },
+          769: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+          320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+        },
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+  })();
+
+  (function () {
+
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+    });
+
+  })();
